9f280a7914a6dc1011b7468c3b097c4ccba8d926
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-05 21:02:00 +0300

    Introduce stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: [
+        'stylelint-config-standard',
+    ],
+    rules: {
+        'alpha-value-notation': 'number',
+        'color-function-notation': 'legacy',
+        'color-hex-length': 'long',
+        'custom-property-pattern': null,
+        'function-url-quotes': 'never',
+        'selector-class-pattern': null,
+    },
+};
