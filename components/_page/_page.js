ff8636d76785414abf9c8a18d450c33a6554af3a
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-05 08:04:34 +0300

    Handle API error

--- components/_page/_page.js
+++ components/_page/_page.js
@@ -1,12 +1,16 @@
 async function _page() {
-    const res = await fetch('https://demo5394722.mockable.io/cars');
-    const data = await res.json();
+    try {
+        const res = await fetch('https://demo5394722.mockable.io/cars');
+        const data = await res.json();
 
-    return catalogContent({
-        content: catalogItemsGallery({
-            items: data,
-        }),
-        button: catalogMore()
-    });
+        return catalogContent({
+            content: catalogItemsGallery({
+                items: data,
+            }),
+            button: catalogMore()
+        });
+    } catch (ex) {
+        return 'Не удалось загрузить данные';
+    }
 }
 
