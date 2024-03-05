e283f1eae1032c765ca0a08f505e1f7de22a2238
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-05 19:47:52 +0300

    Introduce eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'airbnb-base',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'indent': ['error', 4, {ignoredNodes: ['TemplateLiteral *']}],
+        'no-multiple-empty-lines': ['error', {max: 2, maxEOF: 0}],
+        'no-param-reassign': ['error', {props: false}],
+        'no-underscore-dangle': 'off',
+        'no-use-before-define': ['error', {functions: false}],
+        'object-curly-spacing': ['error', 'never'],
+        'quote-props': ['error', 'consistent-as-needed'],
+    },
+};
