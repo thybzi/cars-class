74c15afe77811acf71d9235f77cf2171745f1d9b
Evgeni Dmitriev <thybzi@gmail.com>
2024-03-06 13:40:24 +0300

    Introduce icon params

--- components/icon/icon.js
+++ components/icon/icon.js
@@ -4,6 +4,9 @@
 <div class="icon icon_people catalogItem__detailsItemIcon"></div>
 */
 
-export function icon() {
-    return '<div class="icon icon_heartOutline"></div>';
+export function icon({
+    name,
+    auxClass = '',
+}) {
+    return `<div class="icon icon_${name} ${auxClass}"></div>`;
 }
