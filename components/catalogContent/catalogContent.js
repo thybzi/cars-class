f368250154cdea5fcc1e7be0f51e666e91ee5f4e
Evgeni Dmitriev <edmitriev@ivi.ru>
2024-03-06 23:23:40 +0300

    Load more items by button press

--- components/catalogContent/catalogContent.js
+++ components/catalogContent/catalogContent.js
@@ -1,10 +1,15 @@
-import {catalogItemsGallery} from '../catalogItemsGallery/catalogItemsGallery.js';
+import {addHandler} from '../../handlers.js';
+import {
+    catalogItemsGallery,
+    formatItems as formatGalleryItems,
+} from '../catalogItemsGallery/catalogItemsGallery.js';
 import {catalogMore} from '../catalogMore/catalogMore.js';
 
 export function catalogContent({
     galleryItems = [],
 }) {
     const galleryItemsLimit = 3;
+    addHandler('.catalogMore__button', showMoreItems);
 
     return `
         <div class="catalogContent">
@@ -14,4 +19,15 @@ export function catalogContent({
             ${catalogMore()}
         </div>
     `;
+
+
+    function showMoreItems() {
+        const galleryElem = document.querySelector('.catalogItemsGallery');
+        const lastChildIndex = galleryElem.children.length - 1;
+        const newItems = galleryItems.slice(lastChildIndex, lastChildIndex + galleryItemsLimit);
+
+        if (newItems.length) {
+            galleryElem.insertAdjacentHTML('beforeend', formatGalleryItems(newItems));
+        }
+    }
 }
